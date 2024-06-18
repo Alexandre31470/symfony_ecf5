5e909ecf7c2d8cd35f1f11ab60014ba6f21f1c80
Alexandre31470 <alexandre.quev@gmail.com>
2024-06-18 15:47:10 +0200

    mig

---
+++ src/Entity/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
+    private $title;
+
+    #[ORM\Column(type: 'text')]
+    #[Assert\NotBlank]
+    private $content;
+
+    #[ORM\Column(type: 'datetime')]
+    private $createdAt;
+
+    // Getters et setters...
+}
